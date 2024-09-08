094edaa5d459e3151ece11f38c2f83b9ac5e7847
Aleyana McLeod <aleyanam@usf.edu>
2024-09-08 13:24:38 -0400

    Create and test calculateTip function

--- bill_splitter.js
+++ bill_splitter.js
@@ -5,3 +5,10 @@ let tip = bill > 100 ? .25 : .15; // Calculates the tip based on the price of th
 //Task 2
 // Outputs the bill including the cost of the bill, the tip and the overall cost
 console.log(`The bill was $${bill} , the tip was $${bill *tip}, and the total value is $${(bill *tip) + bill}`);
+
+//Task 3
+function calculateTip(bill,tip){ //Declares the function
+    tip = bill >= 150 ? .25 : .15 ;
+    return (bill *tip); //Function should return the value obtained from performing the calculation
+}
+console.log(`The cost of your tip will be $${calculateTip(100,tip)}`); //Outputs the cost of the tip
